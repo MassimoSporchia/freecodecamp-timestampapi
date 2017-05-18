0ee1c85f52e24f550d9d85cfd5c4c9c7cacd9036
Massimo Sporchia <massimosporchia@gmail.com>
2017-05-18 15:04:08 +0000

    first challenge :bowtie:

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
-app.get('/',function(req, res){
-    res.end('Hello World');
-})
+app.get('/*',function(req,res){
+   var p0 = req.params[0];
+   if ( p0 != null){
+       try{
+        var date = new Date(p0)   
+       }catch(err)
+       {
+           res.send({unix: null, natural: null});
+            res.end();
+       }
+
+       res.send ( {unix: date.getTime(), natural: date})
+       res.end();
+   }else{
+
+   res.send({unix: null, natural: null});
+            res.end();
+   }
+   
+});
+
 app.listen(8080);
